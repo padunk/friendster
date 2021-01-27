399f4458eba4d259c2965a756ee5e1d9e1d683c8
padunk <abraham.agel@gmail.com>
2021-01-27 16:10:16 +0700

    pkg: add tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
-}
+    plugins: [
+        require("tailwindcss")(
+            "./app/javascript/stylesheets/tailwind.config.js"
+        ),
+        require("postcss-import"),
+        require("postcss-flexbugs-fixes"),
+        require("postcss-preset-env")({
+            autoprefixer: {
+                flexbox: "no-2009",
+            },
+            stage: 3,
+        }),
+    ],
+};
